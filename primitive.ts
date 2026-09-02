bb3268273411eeda589ce5745bcdcc34cbc53ff1
Stane911 <rafastane@gmail.com>
2026-09-02 12:46:57 +0530

    Add files via upload

---
+++ primitive.ts
@@ -0,0 +1,15 @@
+let userName = "stane"
+let companyName ="testleaf"
+let contactName = 87454213265
+let knownAutomation =true
+let userPlaywright 
+console.log(userName)
+console.log(typeof userName)
+console.log(companyName)
+console.log(typeof companyName)
+console.log(contactName)
+console.log(typeof contactName)
+console.log(knownAutomation)
+console.log(typeof knownAutomation)
+console.log(userPlaywright)
+console.log(typeof userPlaywright)
