5675a0a403c07918a8011b8328b37523c31099b7
Stane911 <rafastane@gmail.com>
2026-09-03 12:42:36 +0530

    Add files via upload

---
+++ OddorEven.ts
@@ -0,0 +1,15 @@
+let result = isOddOrEven(6);
+
+function isOddOrEven(number)
+{
+    if(number % 2 === 0)
+    {
+        return "Even";
+    }
+    else
+    {
+        return "Odd";
+    }
+}
+console.log(result);
+
